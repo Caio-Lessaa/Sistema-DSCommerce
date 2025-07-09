3375d2b3b53626b0b0d4be96d5bf6c5422b14e53
Caio-Lessaa <caio.lessa01@fatec.sp.gov.br>
2025-07-09 09:08:14 -0300

    GET Products Paged

--- src/main/java/com/caioLessa/dscommerce/controllers/ProductController.java
+++ src/main/java/com/caioLessa/dscommerce/controllers/ProductController.java
@@ -1,12 +1,15 @@
 package com.caioLessa.dscommerce.controllers;
 
 import com.caioLessa.dscommerce.dto.ProductDTO;
-import com.caioLessa.dscommerce.repositories.ProductRepository;
 import com.caioLessa.dscommerce.service.ProductService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -19,6 +22,11 @@ public class ProductController {
 
     @GetMapping("/{id}")
     public ProductDTO findById(@PathVariable Long id) {
-        return productService.findById(id); 
+        return productService.findById(id);
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return productService.findAll(pageable);
     }
 }
--- src/main/java/com/caioLessa/dscommerce/service/ProductService.java
+++ src/main/java/com/caioLessa/dscommerce/service/ProductService.java
@@ -3,20 +3,29 @@ package com.caioLessa.dscommerce.service;
 import com.caioLessa.dscommerce.dto.ProductDTO;
 import com.caioLessa.dscommerce.entities.Product;
 import com.caioLessa.dscommerce.repositories.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class ProductService {
-    private ProductRepository repository;
+    private ProductRepository productRepository;
 
-    public ProductService(ProductRepository repository) {
-        this.repository = repository;
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
-        Product product = repository.findById(id).orElseThrow(() -> new RuntimeException("Produto não encontrado para o ID informado!"));
+        Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Produto não encontrado para o ID informado!"));
         return new ProductDTO(product);
+    }
+
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = productRepository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
 }
